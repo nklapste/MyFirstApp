4ac2f09a7fe9eb515d7eca7a58e37979aa05418c
nklapste <nklapste@ualberta.ca>
2018-09-06 13:07:42 -0600

    adding logging

--- app/src/main/java/ca/klapstein/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/ca/klapstein/myfirstapp/DisplayMessageActivity.java
@@ -3,9 +3,11 @@ package ca.klapstein.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+    private static final String TAG = "DisplayMessageActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        Log.d(TAG, "Displaying message: " + message);
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
