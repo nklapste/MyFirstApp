39bad32396ed965f61d57e465934b2d6eea9f6ff
nklapste <nklapste@ualberta.ca>
2018-09-06 13:10:20 -0600

    refactoring

--- app/src/main/java/ca/klapstein/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/ca/klapstein/myfirstapp/DisplayMessageActivity.java
@@ -1,8 +1,8 @@
 package ca.klapstein.myfirstapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
--- app/src/main/java/ca/klapstein/myfirstapp/MainActivity.java
+++ app/src/main/java/ca/klapstein/myfirstapp/MainActivity.java
@@ -1,15 +1,15 @@
 package ca.klapstein.myfirstapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
